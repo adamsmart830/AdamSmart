5565f0c855ce7208877d8f980a9fa0948ba9cb17
Smart <smarta2@rpi.edu>
2023-03-21 10:47:23 -0400

    Button Fix

---
+++ How2Website/FinalWebsite/javascript/java.js
@@ -0,0 +1,4 @@
+function sound(){
+     var snd = new Audio('../sounds/key01.mp3')//wav is also supported
+     snd.play()//plays the sound
+}
--- How2Website/FinalWebsite/javascrpit/java.js
+++
@@ -1,8 +0,0 @@
-const audio = new Audio("../sounds/key01.mp3");
-cost buttons = document.querySelectorAll("Button");
-
-buttons.forEach(button => {
-	button.addEventListener("click", () => {
-		audio.play();
-	});
-});
