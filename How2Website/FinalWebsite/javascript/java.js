79e7bc8ad84ca56e979d47cb393041a7b13a68bb
Smart <smarta2@rpi.edu>
2023-03-24 16:48:34 -0400

    Added Intermediate pages and image mapping

--- How2Website/FinalWebsite/javascript/java.js
+++ How2Website/FinalWebsite/javascript/java.js
@@ -1,4 +1,5 @@
 function sound(){
      var snd = new Audio('../sounds/key01.mp3')//wav is also supported
      snd.play()//plays the sound
+     setTimeout("location.href = '../html/1MainPage.html';", 1500);
 }
